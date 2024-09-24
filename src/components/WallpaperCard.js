1a0c97c14c46168b8380e112530099006366118b
esc4n0rx <paulo.cunha@hortifruti.com.br>
2024-09-24 12:16:12 -0300

    Fixado css

--- src/components/WallpaperCard.js
+++ src/components/WallpaperCard.js
@@ -11,13 +11,18 @@ const WallpaperCard = ({ wallpaper }) => {
   return (
     <>
       <div className="card" onClick={toggleModal}>
-        <img className="thumbnail" src={wallpaper.links.download} alt={wallpaper.alt_description} loading="lazy" />
+        <img
+          className="thumbnail"
+          src={wallpaper.urls.regular}
+          alt={wallpaper.alt_description}
+          loading="lazy"
+        />
       </div>
 
       {isModalOpen && (
         <div className={`modal ${isModalOpen ? 'active' : ''}`}>
           <div className="modal-content">
-            <button className="close-modal" onClick={toggleModal}>X</button> 
+            <button className="close-modal" onClick={toggleModal}>X</button>
             <h2>{wallpaper.description || "Sem descrição"}</h2>
             <p>❤️ {wallpaper.likes} likes</p>
             <a href={wallpaper.links.download} target="_blank" rel="noopener noreferrer">
